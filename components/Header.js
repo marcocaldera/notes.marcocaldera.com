41e12e3d86d4a9c3f73f3e692aa945992aa979cc
Marco Caldera <marco.caldera93@icloud.com>
2022-12-27 12:46:56 +0100

    feat: add logo

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,13 @@
+import Logo from "./Logo";
+
 export default function Header(props) {
 	return (
 		<header className="header">
 			<nav className="nav" role="navigation" aria-label="main navigation">
 				<div className="header__container">
-					<a href="/" className="header__container__logo underlined">
-						{props.siteTitle}
+					<a href="/" className="header__container">
+						{/* {props.siteTitle} */}
+						<Logo />
 					</a>
 				</div>
 				<ul className="header__links">
@@ -13,11 +16,11 @@ export default function Header(props) {
 							Notes
 						</a>
 					</li>
-					<li>
+					{/* <li>
 						<a href="/contact" className="underlined">
 							Contact
 						</a>
-					</li>
+					</li> */}
 					<li>
 						<a href="https://marcocaldera.com" target="_blank">
 							Website
