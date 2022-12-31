19fbbfd99a33b90d85cdf3a6b351cf10c0007e78
Marco Caldera <marco.caldera93@icloud.com>
2022-12-31 10:27:34 +0100

    fix: gen post file

--- gen-post.sh
+++ gen-post.sh
@@ -2,20 +2,20 @@
 
 # This script generates a new blog post.
 # Example usage:
-# ./contrib/gen-post.sh My first post
+# ./gen-post.sh My first post
 
 DATE=$(date +"%Y-%m-%d")
-TITLE="$@" 
+TITLE="$@"
 FILE_TITLE=$(printf "$TITLE" | tr " " "-" | tr "[A-Z]" "[a-z]")
 FILE_NAME="$DATE-$FILE_TITLE.md"
 
 FULL_PATH="content/posts/$FILE_NAME"
 
-cat > $FULL_PATH <<EOF
+cat > $FULL_PATH <<EOFs
 ---
 title: $TITLE
 date: "$DATE"
-tags: "note, 100DaysToOffload"
+tags: "engineering, 100DaysToOffload"
 ---
 
 EOF
